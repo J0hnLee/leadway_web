f4555b3b0542eafcc77a560ddedcb25fab36a8a3
John <john791016@gmail.com>
2023-06-13 11:54:21 +0800

    move tutorial to temp_doc, delete hover animation,

--- src/components/HomepageFeatures/index.tsx
+++ src/components/HomepageFeatures/index.tsx
@@ -10,22 +10,20 @@ type FeatureItem = {
 
 const FeatureList: FeatureItem[] = [
   {
-    title: '寵物友善',
+    title: '專注健康',
     Svg: require('@site/static/img/undraw_docusaurus_mountain.svg').default,
     description: (
       <>
-        Docusaurus was designed from the ground up to be easily installed and
-        used to get your website up and running quickly.
+        理維的藥師熱衷於追求健康，致力於將最新技術應用於醫療產業，為民眾帶來卓越的醫療體驗。我們提供最新國際知識，專注於健康科技和數位健康，為未來十年帶來更多可能。
       </>
     ),
   },
   {
-    title: '專注在健康',
+    title: '維繫醫病，延伸關懷',
     Svg: require('@site/static/img/undraw_docusaurus_tree.svg').default,
     description: (
       <>
-        Docusaurus lets you focus on your docs, and we&apos;ll do the chores. Go
-        ahead and move your docs into the <code>docs</code> directory.
+        醫療人員與患者建立連結，不僅在診所或藥局，還能在家中延續關懷，提供個別化的建議。
       </>
     ),
   },
@@ -34,8 +32,13 @@ const FeatureList: FeatureItem[] = [
     Svg: require('@site/static/img/undraw_docusaurus_react.svg').default,
     description: (
       <>
-        Extend or customize your website layout by reusing React. Docusaurus can
-        be extended while reusing the same header and footer.
+        醫療賦能的目標是透過技術和知識的傳遞，提升醫療人員的能力和專業水準，
+        同時鼓勵更多人加入醫療領域，攜手為健康服務做出貢獻。
+        透過新興科技和數位化平台，醫療人員可以獲得更全面、即時的資訊，加強診斷和治療的準確性，
+        提高醫療效率和品質。此外，這也鼓勵更多人投身於醫療事業，
+        透過專業的培訓和創新的機會，培養更多優秀的醫護人才，
+        以滿足人們對醫療服務不斷增長的需求。這種醫療賦能的模式將帶來更健康的社會，
+        讓更多人享受到高品質的醫療照護，建立起更強大的健康社區。
       </>
     ),
   },
