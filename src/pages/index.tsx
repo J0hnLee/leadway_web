246fee3cb09a6113e758f6ca3921607e0b98df20
John <john791016@gmail.com>
2023-10-26 01:49:03 +0800

    change heroSection

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,25 +1,34 @@
-import React from 'react';
-import clsx from 'clsx';
-import Link from '@docusaurus/Link';
-import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
-import Layout from '@theme/Layout';
-import HomepageFeatures from '@site/src/components/HomepageFeatures';
-import '@docsearch/css';
+import React from "react";
+import clsx from "clsx";
+import Link from "@docusaurus/Link";
+import useDocusaurusContext from "@docusaurus/useDocusaurusContext";
+import Layout from "@theme/Layout";
+import HomepageFeatures from "@site/src/components/HomepageFeatures";
+import "@docsearch/css";
+import { TypeAnimation } from "react-type-animation";
 
-import styles from './index.module.css';
+import styles from "./index.module.css";
 
 function HomepageHeader() {
-  const {siteConfig} = useDocusaurusContext();
+  const { siteConfig } = useDocusaurusContext();
   return (
-    <header className={clsx('hero hero--primary', styles.heroBanner)}>
-      
+    <header className={clsx("hero hero--primary", styles.heroBanner)}>
       <div className="container">
+        <TypeAnimation
+          className="hero__title"
+          sequence={["Leadway", 2000, "因為理解", 2000, "所以維繫", 2000]}
+          wrapper="span"
+          speed={50}
+          repeat={Infinity}
+        />
         <h1 className="hero__title">{siteConfig.title}</h1>
         <p className="hero__subtitle ">{siteConfig.tagline}</p>
+
         <div className={styles.buttons}>
           <Link
             className="button button--secondary button--lg"
-            to="/docs/intro">
+            to="/docs/intro"
+          >
             你的醫療領路人🚶
           </Link>
           {/* <DocSearch
@@ -34,11 +43,12 @@ function HomepageHeader() {
 }
 
 export default function Home(): JSX.Element {
-  const {siteConfig} = useDocusaurusContext();
+  const { siteConfig } = useDocusaurusContext();
   return (
     <Layout
       title={`Hello from ${siteConfig.title}`}
-      description="Description will go into a meta tag in <head />">
+      description="Description will go into a meta tag in <head />"
+    >
       <HomepageHeader />
       <main>
         <HomepageFeatures />
